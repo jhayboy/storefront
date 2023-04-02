7ea1d72344bb05c84ef07adfb4af9ceaf90f9fd9
jhayboy <mbamarajustice1@gmail.com>
2023-04-02 17:22:29 +0100

    line

--- src/Components/SneakersData.js
+++ src/Components/SneakersData.js
@@ -4,7 +4,7 @@ export const sneak = [
         images: "https://i.pinimg.com/236x/4d/a3/e6/4da3e69a4ad288f8afb2f88beb660954.jpg",
         name: "Curvy Abstract Pattern",
         price: 53000,
-        sold: 192,
+        sold: 194,
         types: "frames",
         size: "30 by 30",
         description: "Curvy Abstract Pattern Multi Panel Canvas Wall Art bring unique style into your home. Change the entire feel of any space and make an interesting statement with this abstract canvas print."
@@ -14,7 +14,7 @@ export const sneak = [
         images: "https://i.pinimg.com/564x/b7/50/2d/b7502d084823e78869303f5fe0cefcbc.jpg",
         name: "Circles Wall Art",
         price: 42200,
-        sold: 115,
+        sold: 117,
         types: "frames",
         size: "20 by 28",
         description: "A simple canvas print suitable for both living room and bedroom, will be Delivered as seen in the image"
@@ -24,7 +24,7 @@ export const sneak = [
         images: "https://i.pinimg.com/564x/06/9e/c2/069ec215e7237aab1035316e7d4cdaf5.jpg",
         name: "Modern Yellow n White Abstract",
         price: 35000,
-        sold: 39,
+        sold: 40,
         types: "frames",
         size: "24 by 24",
         description: "A stylish and versatile addition to your wardrobe. Made with a soft and comfortable cotton blend, this hoodie is perfect for keeping you cozy and warm on cooler days."
@@ -34,7 +34,7 @@ export const sneak = [
         images: "https://i.pinimg.com/564x/d2/ed/e6/d2ede63f6efa53e6a5650ded7b8db9ae.jpg",
         name: "Yellow Eye Woman",
         price: 24200,
-        sold: 121,
+        sold: 122,
         types: "frames",
         size: "16 by 24",
         description: "One of the best canvas art print for both living room and bedroom"
@@ -53,7 +53,7 @@ export const sneak = [
         images: "https://i.pinimg.com/564x/91/3b/8b/913b8b7236eed833637613164192bf6a.jpg",
         name: "mop paintings",
         price: 180000,
-        sold: 5,
+        sold: 7,
         types: "frames",
         size: "36 by 36",
         description: "The resulting artwork is vibrant, energetic, and expressive, with layers of color and texture built up through repeated mopping motions."
@@ -63,7 +63,7 @@ export const sneak = [
         images: "https://i.pinimg.com/236x/34/3d/06/343d066fd9b9be4fac6646707b80090c.jpg",
         name: "Modern Abstract Figure",
         price: 28000,
-        sold: 123,
+        sold: 125,
         types: "frames",
         size: "16 by 24",
         description: "Buy Modern Abstract Figure Face Geometric Canvas Poster Prints Wall Art Pictures For Living Room Bedroom Home Decor. Buy Now"
@@ -82,7 +82,7 @@ export const sneak = [
         images: "https://i.pinimg.com/564x/f4/db/c4/f4dbc4ab8178ccd8b67067a17004e9b4.jpg",
         name: "Asymmetrical Mirror",
         price: 65600,
-        sold: 43,
+        sold: 44,
         types: "mirror",
         size: " height 48' width 24'",
         description: "The more time you spend at home, the more you find ways to make it feel personal and inviting. "
@@ -102,7 +102,7 @@ export const sneak = [
         images: "https://i.pinimg.com/564x/32/a3/eb/32a3eb3043ae9df4e5d4f92de961a9b5.jpg",
         name: "Large Asymmetrical Wall Modern Mirror",
         price: 80000,
-        sold: 60,
+        sold: 61,
         types: "mirror",
         size: " height 24' width 36'",
         description: "You can use that asymmetric black framed mirror as decorative in many parts of your home. it's up to your imagination Multipurpose Wavy Mirror : Asymmetrical large mirror the clean lines and sleek design creates a dimensional delight to any living space whether you place it gently in a corner or hang on your wall in combination with other accents. In many homes, the most simple accents create the most powerful impressions. The asymmetrical Irregular mirror, widely used in every. "
@@ -112,7 +112,7 @@ export const sneak = [
         images: "https://i.pinimg.com/564x/f6/31/51/f63151432785523f061a5d5088bbeffc.jpg",
         name: "Push & Pull Wall Art | How To Hide a Fuse Panel",
         price: 28000,
-        sold: 25,
+        sold: 30,
         types: "frames",
         size: " height 16' width 24'",
         description: "Large canvas wall art turns any blank area into an eye-catching display, all while lending gallery-worthy appeal to your abode. Just take a look at our vintage hangings for example. Each piece of home decor paintings is printed on canvas, then wrapped around a frame. Wall art prints arrive ready to hang so you can brighten up your room in several minutes."
@@ -132,7 +132,7 @@ export const sneak = [
         images: "https://i.pinimg.com/564x/0e/fa/7e/0efa7e39de0a59894331a9dd86ece3d4.jpg",
         name: "Golden Areal Push & Pull Wall Art",
         price: 35000,
-        sold: 22,
+        sold: 24,
         types: "open",
         size: " height 20' width 24'",
         description: "Large canvas wall art turns any blank area into an eye-catching display, all while lending gallery-worthy appeal to your abode."
@@ -152,9 +152,19 @@ export const sneak = [
         images: "https://i.pinimg.com/564x/4e/fa/e4/4efae497b79c43ba2f2805e88072ffbf.jpg",
         name: "Golden Areal Push & Pull Wall Art Decor",
         price: 35000,
-        sold: 49,
+        sold: 50,
         types: "open",
         size: " height 16' width 24'",
         description: "Large canvas wall art turns any blank area into an eye-catching display, all while lending gallery-worthy appeal to your abode. Just take a look at our vintage hangings for example. Each piece of home decor paintings is printed on canvas, then wrapped around a frame. Wall art prints arrive ready to hang so you can brighten up your room in several minutes."
+    },
+    {
+        id: "17",
+        images: "https://i.pinimg.com/564x/38/ba/f2/38baf21c855107a6bb92d93e67ac7a76.jpg",
+        name: "One line gold abstract",
+        price: 28000,
+        sold: 5,
+        types: "frames",
+        size: " height 16' width 24'",
+        description: "Minimalistic and striking, 'One Line Abstract' is an impressive artwork created using a single continuous line, showcasing the artist's mastery of composition and precision."
     },
 ]
